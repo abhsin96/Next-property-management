015fca16da8589fccf65791e4f586ec859952e42
Abhishek Singh <abhsin96@gmail.com>
2025-03-08 14:41:15 +0530

    config updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
+require("dotenv").config();
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -22,12 +22,23 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  env: {
+  publicRuntimeConfig: {
     NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
     NEXT_PUBLIC_API_DOMAIN: process.env.NEXT_PUBLIC_API_DOMAIN,
     NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY:
       process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
     NEXT_PUBLIC_MAPBOX_API_KEY: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
+  },
+  serverRuntimeConfig: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL_INTERNAL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    CLOUDNARY_CLOUD_NAME: process.env.CLOUDNARY_CLOUD_NAME,
+    CLOUDNARY_SECRET: process.env.CLOUDNARY_SECRET,
+    CLOUDNARY_API_KEY: process.env.CLOUDNARY_API_KEY,
   },
 };
 
